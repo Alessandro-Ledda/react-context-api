717fe625c876d568e7b57c529388a3f3133cc1db
Alessandro Ledda <aleledda75@icloud.com>
2026-01-30 18:07:18 +0100

    feat navabr

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+
+    return (
+        <nav className="container-nav">
+            <div className="nav-content">
+                <button className="btn">Attiva Modalità Budget</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
